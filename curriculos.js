89337eb2a883e5b1e77fce73c5476092daf53c0f
ryanvpacheco <r.pacheco.o777@gmail.com>
2023-05-28 21:16:29 -0300

    Adicionando Nova Pagina Prototipo

---
+++ curriculos.js
@@ -0,0 +1,67 @@
+// Função para exibir o conteúdo da aba correspondente
+function showTabContent(tab) {
+    // Obter todos os elementos de conteúdo das abas
+    const tabContents = document.querySelectorAll('.tab-content');
+  
+    // Ocultar todos os conteúdos das abas
+    tabContents.forEach(content => {
+      content.style.display = 'none';
+    });
+  
+    // Exibir o conteúdo da aba selecionada
+    const selectedContent = document.querySelector(`.tab-content[data-tab="${tab}"]`);
+    if (selectedContent) {
+      selectedContent.style.display = 'block';
+    }
+  }
+  
+  // Função para mudar a aparência do botão da aba selecionada
+  function setActiveTabButton(button) {
+    // Remover a classe "active" de todos os botões das abas
+    const tabButtons = document.querySelectorAll('.tab-card');
+    tabButtons.forEach(btn => {
+      btn.classList.remove('active');
+    });
+  
+    // Adicionar a classe "active" ao botão da aba selecionada
+    button.classList.add('active');
+  }
+  
+  // Função para fechar a aba após 1 minuto de inatividade
+  function closeTabAfterTimeout(tab) {
+    let timeout;
+    const tabContent = document.querySelector(`.tab-content[data-tab="${tab}"]`);
+  
+    // Reiniciar o temporizador a cada interação
+    tabContent.addEventListener('mouseover', resetTimeout);
+    tabContent.addEventListener('touchstart', resetTimeout);
+  
+    function resetTimeout() {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        showTabContent('');
+        setActiveTabButton(null);
+      }, 60000); // 1 minuto
+    }
+  }
+  
+  // Obter todos os botões das abas
+  const tabButtons = document.querySelectorAll('.tab-card');
+  
+  // Adicionar eventos de clique aos botões das abas
+  tabButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Obter o valor do atributo "data-tab" do botão da aba
+      const tab = button.getAttribute('data-tab');
+  
+      // Exibir o conteúdo da aba correspondente
+      showTabContent(tab);
+  
+      // Mudar a aparência do botão da aba selecionada
+      setActiveTabButton(button);
+  
+      // Fechar a aba após 1 minuto de inatividade
+      closeTabAfterTimeout(tab);
+    });
+  });
+  
--- script.js
+++ script.js
@@ -1,23 +1,59 @@
-// Função para virar o card ao passar o mouse
+// Função para virar o card
 function flipCard(element) {
-    element.classList.toggle('flip');
-}
-
-// Função para redirecionar para o currículo ao clicar no card
-function redirectToCurriculo(element) {
+    element.classList.add('flip');
+  }
+  
+  // Função para reverter o card após 3 segundos de inatividade do mouse ou do toque
+  function resetCard(element) {
+    element.classList.remove('flip');
+  }
+  
+  // Função para redirecionar para o currículo ao clicar no card
+  function redirectToCurriculo(element) {
     var curriculoURL = element.getAttribute('data-href');
     window.location.href = curriculoURL;
-}
-
-// Adicionar classe ao footer quando chegar ao final da página
-window.addEventListener('scroll', function() {
-    var footer = document.querySelector('.footer');
-    var scrollPosition = window.innerHeight + window.pageYOffset;
-    var footerPosition = document.body.offsetHeight - footer.offsetHeight;
-
-    if (scrollPosition >= footerPosition) {
-        footer.classList.add('show');
-    } else {
-        footer.classList.remove('show');
+  }
+  
+  // Adicionar eventos de interação aos cards
+  var curriculos = document.querySelectorAll('.curriculo');
+  curriculos.forEach(function(curriculo) {
+    var timeout;
+    var isTouchDevice = false;
+  
+    // Verificar se é um dispositivo com tela sensível ao toque
+    if ('ontouchstart' in window || navigator.maxTouchPoints) {
+      isTouchDevice = true;
     }
-});
+  
+    // Adicionar eventos de acordo com o tipo de dispositivo
+    if (isTouchDevice) {
+      curriculo.addEventListener('touchstart', function() {
+        flipCard(this);
+      });
+  
+      curriculo.addEventListener('touchend', function() {
+        var cardElement = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+          resetCard(cardElement);
+        }, 3000);
+      });
+    } else {
+      curriculo.addEventListener('mouseenter', function() {
+        flipCard(this);
+      });
+  
+      curriculo.addEventListener('mouseleave', function() {
+        var cardElement = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+          resetCard(cardElement);
+        }, 3000);
+      });
+    }
+  
+    curriculo.addEventListener('click', function() {
+      redirectToCurriculo(this);
+    });
+  });
+  
