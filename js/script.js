374c1e30f72bc339a3846de449a4f074e6c25448
Ryan Pacheco <102881907+ryanvpacheco@users.noreply.github.com>
2023-06-04 17:00:22 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-// Função para virar o card
 function flipCard(element) {
   element.classList.add('flip');
 }
 
-// Função para reverter o card
+// Função para reverter o card após 3 segundos de inatividade do mouse ou do toque
 function resetCard(element) {
   element.classList.remove('flip');
 }
@@ -17,33 +16,42 @@ function redirectToCurriculo(element) {
 // Adicionar eventos de interação aos cards
 var curriculos = document.querySelectorAll('.curriculo');
 curriculos.forEach(function (curriculo) {
+  var timeout;
   var isTouchDevice = false;
 
   // Verificar se é um dispositivo com tela sensível ao toque
   if ('ontouchstart' in window || navigator.maxTouchPoints) {
-    isTouchDevice = true;
+      isTouchDevice = true;
   }
 
   // Adicionar eventos de acordo com o tipo de dispositivo
   if (isTouchDevice) {
-    curriculo.addEventListener('touchstart', function () {
-      flipCard(this);
-    });
+      curriculo.addEventListener('touchstart', function () {
+          flipCard(this);
+      });
 
-    curriculo.addEventListener('touchend', function () {
-      var cardElement = this;
-      setTimeout(function () {
-        resetCard(cardElement);
-      }, 3000);
-    });
+      curriculo.addEventListener('touchend', function () {
+          var cardElement = this;
+          clearTimeout(timeout);
+          timeout = setTimeout(function () {
+              resetCard(cardElement);
+          }, 3000);
+      });
   } else {
-    curriculo.addEventListener('click', function () {
-      flipCard(this);
-      redirectToCurriculo(this);
-    });
+      curriculo.addEventListener('mouseenter', function () {
+          flipCard(this);
+      });
 
-    curriculo.addEventListener('mouseleave', function () {
-      resetCard(this);
-    });
+      curriculo.addEventListener('mouseleave', function () {
+          var cardElement = this;
+          clearTimeout(timeout);
+          timeout = setTimeout(function () {
+              resetCard(cardElement);
+          }, 3000);
+      });
   }
+
+  curriculo.addEventListener('click', function () {
+      redirectToCurriculo(this);
+  });
 });
